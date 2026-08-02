83389f819e1d3697c8098662cf2ba6b48a11049c
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-02 08:58:43 +0530

    Time: 4 ms (87.93%), Space: 46.7 MB (35.51%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String convert(String s, int numRows) {
+
+        // Edge case
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        // Initialize each row
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+
+            rows[currentRow].append(c);
+
+            // Change direction at first and last row
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        // Combine all rows
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
