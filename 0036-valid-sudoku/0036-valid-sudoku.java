31bd33e7d430a7f239a59d5747e40edbd3706bfa
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-10 00:08:07 +0530

    Time: 1 ms (99.92%), Space: 46.2 MB (93.47%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                char ch = board[row][col];
+
+                // Ignore empty cells
+                if (ch == '.') {
+                    continue;
+                }
+
+                int digit = ch - '1';
+
+                // Find the 3x3 box number
+                int box = (row / 3) * 3 + (col / 3);
+
+                // Check duplicate
+                if (rows[row][digit] ||
+                    cols[col][digit] ||
+                    boxes[box][digit]) {
+
+                    return false;
+                }
+
+                // Mark digit as used
+                rows[row][digit] = true;
+                cols[col][digit] = true;
+                boxes[box][digit] = true;
+            }
+        }
+
+        return true;
+    }
+}
