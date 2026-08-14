10a746d1186e825b94bb801863745c0eae2b8720
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-14 18:46:50 +0530

    Time: 1 ms (61.42%), Space: 42.4 MB (69.84%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            // Take the last bit of n
+            int bit = n & 1;
+
+            // Shift result left and add the bit
+            result = (result << 1) | bit;
+
+            // Remove the last bit from n
+            n = n >>> 1;
+        }
+
+        return result;
+    }
+}
