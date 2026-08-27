bba1c7b9c62b4764f7fce309399a620cff701140
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-28 00:02:02 +0530

    Time: 193 ms (80.68%), Space: 274.3 MB (58.01%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,73 @@
+class WordDictionary {
+
+    // Trie Node
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    // Add a word to the Trie
+    public void addWord(String word) {
+
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    // Search a word
+    public boolean search(String word) {
+        return dfs(root, word, 0);
+    }
+
+    private boolean dfs(TrieNode node, String word, int index) {
+
+        // Reached the end of the word
+        if (index == word.length()) {
+            return node.isEnd;
+        }
+
+        char c = word.charAt(index);
+
+        // Normal character
+        if (c != '.') {
+
+            int childIndex = c - 'a';
+
+            if (node.children[childIndex] == null) {
+                return false;
+            }
+
+            return dfs(node.children[childIndex], word, index + 1);
+        }
+
+        // '.' can match any character
+        for (int i = 0; i < 26; i++) {
+
+            if (node.children[i] != null) {
+
+                if (dfs(node.children[i], word, index + 1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
