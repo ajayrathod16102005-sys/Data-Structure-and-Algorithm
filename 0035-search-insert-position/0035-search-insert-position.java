7fc1826dfa3bb222b17abeb89fe38b2e67203c3d
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-29 23:37:45 +0530

    Time: 0 ms (100%), Space: 44.6 MB (66.96%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -15,6 +15,8 @@ class Solution {
             if (nums[mid] < target) {
                 left = mid + 1;
             } else {
+
+                
                 right = mid - 1;
             }
         }
