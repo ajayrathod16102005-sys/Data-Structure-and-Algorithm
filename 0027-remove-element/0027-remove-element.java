7c6c84683c240d80ceae6531b552c18569828fec
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-08-29 23:42:53 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -5,7 +5,8 @@ class Solution {
 
         for (int j = 0; j < nums.length; j++) {
 
-            if (nums[j] != val) {
+            if (nums[j] != val)
+             {
                 nums[i] = nums[j];
                 i++;
             }
