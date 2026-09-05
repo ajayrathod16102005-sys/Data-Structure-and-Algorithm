fd8169e7eff104f053066a2e569ce2c8dec52b65
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-05 22:27:26 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,9 @@ class Solution {
             return n;
         }
 
+        
         int prev2 = 1; // ways(1)
+
         int prev1 = 2; // ways(2)
 
         for (int i = 3; i <= n; i++) {
@@ -13,6 +15,7 @@ class Solution {
             int current = prev1 + prev2;
 
             prev2 = prev1;
+            
             prev1 = current;
         }
 
