7e247e45203a68001b7eba0f70fec216e75fb45f
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-06 21:56:12 +0530

    Time: 1 ms (99.99%), Space: 43.7 MB (22.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -14,7 +14,9 @@ class Solution {
         // Handle patterns like a*, a*b*, a*b*c*
         for (int j = 2; j <= n; j++) {
             if (p.charAt(j - 1) == '*') {
+               
                 dp[0][j] = dp[0][j - 2];
+                
             }
         }
 
