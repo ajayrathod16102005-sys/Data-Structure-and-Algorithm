45142ca2671b195f6aa91f4028f1ba8234f21a90
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-10 23:18:20 +0530

    Time: 0 ms (100%), Space: 43.1 MB (43.57%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
 class Solution {
+
     public List<Integer> spiralOrder(int[][] matrix) {
 
         
         List<Integer> result = new ArrayList<>();
 
         int top = 0;
+
         int bottom = matrix.length - 1;
+        
         int left = 0;
         int right = matrix[0].length - 1;
 
