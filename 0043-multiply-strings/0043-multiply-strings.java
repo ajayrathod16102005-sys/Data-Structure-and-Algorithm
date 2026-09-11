bb9880d4bf95bdd26f0f6c5c183829c256aa0977
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-11 23:24:38 +0530

    Time: 3 ms (77.61%), Space: 43.5 MB (63.61%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -16,9 +16,11 @@ class Solution {
         // Multiply each digit
         for (int i = n - 1; i >= 0; i--) {
 
-            for (int j = m - 1; j >= 0; j--) {
+            for (int j = m - 1; j >= 0; j--)
+             {
 
                 int digit1 = num1.charAt(i) - '0';
+                
                 int digit2 = num2.charAt(j) - '0';
 
                 int product = digit1 * digit2;
