f96dc51355c4ac944eca32afb9aaa3ed9cf389ba
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-11 23:23:14 +0530

    Time: 1 ms (99.95%), Space: 71.1 MB (72.37%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -8,13 +8,15 @@ class Solution {
 
             while (nums[i] >= 1 &&
                    nums[i] <= n &&
-                   nums[nums[i] - 1] != nums[i]) {
+                   nums[nums[i] - 1] != nums[i])
+                    {
 
                 int correctIndex = nums[i] - 1;
 
                 int temp = nums[i];
                 
                 nums[i] = nums[correctIndex];
+                
                 nums[correctIndex] = temp;
             }
         }
