71db8975a81a53320c69395ac18c58a9537ab12b
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-17 21:59:40 +0530

    Time: 2 ms (69.42%), Space: 45 MB (12.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -12,12 +12,14 @@ class Solution {
     }
 
     private void backtrack(List<String> result, String current,
-                            int open, int close, int n) {
+                            int open, int close, int n)
+                             {
 
         // A valid combination is complete
         if (current.length() == 2 * n) {
             
             result.add(current);
+            
             return;
         }
 
