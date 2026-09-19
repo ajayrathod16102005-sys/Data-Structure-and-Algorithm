518340a73c8d3127d2ae3fc6e3e6b4745bb34b5c
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-19 21:22:49 +0530

    Time: 2 ms (99.31%), Space: 44.3 MB (73.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,11 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    public boolean isPalindrome(String s)
+     {
         int left = 0;
         int right = s.length() - 1;
 
-        while (left < right) {
+        while (left < right) 
+        {
 
             // Skip non-alphanumeric characters from left
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
