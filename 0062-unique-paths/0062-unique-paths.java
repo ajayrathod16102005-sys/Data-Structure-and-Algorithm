33a8130d01e91739d2e2f54104324a6e90166e44
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-24 22:11:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,9 +16,11 @@ class Solution {
 
         // Fill the remaining cells
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+            for (int j = 1; j < n; j++)
+             {
 
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
             }
         }
 
