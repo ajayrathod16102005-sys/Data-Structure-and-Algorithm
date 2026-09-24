7632fd7d89a9390a622fe30b5f0667c6fa555b0d
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-24 22:05:25 +0530

    Time: 1 ms (98.04%), Space: 47.2 MB (19.5%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -14,9 +14,12 @@ class Solution {
         // 2. Merge overlapping intervals
         while (i < n && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            
             i++;
         }
+
 
         // 3. Add merged interval
         result.add(newInterval);
