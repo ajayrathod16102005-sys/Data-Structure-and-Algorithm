4fbd74353dfc7dcd6407539421705be79f9d6b6e
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-24 21:56:39 +0530

    Time: 6 ms (98.99%), Space: 49.8 MB (34.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+class Solution 
+{
+    public List<List<String>> groupAnagrams(String[] strs)
+     {
 
         HashMap<String, List<String>> map = new HashMap<>();
 
