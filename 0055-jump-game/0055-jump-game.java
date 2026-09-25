4d0a0184654b4fc5861f9a7bbb1e82443d52d21f
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-25 23:22:22 +0530

    Time: 2 ms (89.62%), Space: 48 MB (42.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,8 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+
+    public boolean canJump(int[] nums) 
+    {
+
         int farthest = 0;
 
         for (int i = 0; i < nums.length; i++) {
