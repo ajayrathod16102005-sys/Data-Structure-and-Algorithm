83d5b39f58d7c721c98eb5a930b06faaa9fb60b5
Ajay_1610 <ajayrathod16102005@gmail.com>
2026-09-26 20:37:36 +0530

    Time: 9 ms (33.85%), Space: 49.1 MB (60.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -17,7 +17,6 @@ class Solution {
 
             // Overlapping intervals
             if (intervals[i][0] <= end) {
-               
                 end = Math.max(end, intervals[i][1]);
             } 
             // Non-overlapping interval
